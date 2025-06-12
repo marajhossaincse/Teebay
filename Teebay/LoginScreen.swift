42bfeba85dae5a11e9b4214ccdd0e2ae942a9180
Maraz Hossain <maraz_hossain@apple.com>
2025-06-12 12:49:56 +0600

    implement login ui.

--- Teebay/LoginScreen.swift
+++ Teebay/LoginScreen.swift
@@ -3,7 +3,7 @@
 //  Teebay
 //
 //  Created by Maraj Hossain on 11/6/25.
-//  
+//
 
 import SwiftUI
 
@@ -12,33 +12,50 @@ struct LoginScreen: View {
     @State var password: String
     
     var body: some View {
-        VStack{
-            Text("SIGN IN")
-            
+        VStack(spacing: 40) {
             Spacer()
             
+            Text("SIGN IN")
+                .font(.title)
+            
             TextField("Email", text: $email)
-                .padding()
+                .padding(10)
+                .background(Color.white)
+                .overlay(
+                    Rectangle()
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .foregroundColor(.gray)
                 
             TextField("Password", text: $password)
-                .padding()
+                .padding(10)
+                .background(Color.white)
+                .overlay(
+                    Rectangle()
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .foregroundColor(.gray)
             
             Button {} label: {
                 Text("LOGIN")
             }
             .padding()
-            .background(Color.green)
+            .background(Color.purple)
             .foregroundColor(.white)
-            .cornerRadius(20)
+            
+            Text("Don't have an account? Signup")
+    
+            Spacer()
         }
+        .padding(40)
     }
 }
 
 struct LoginScreen_Previews: PreviewProvider {
     static var previews: some View {
         LoginScreen(
-            email: "Lorem Ipsum",
-            password: "Ipsum"
+            email: "",
+            password: ""
         )
     }
 }
